6fa135b4d222ce7fa083c1c201f84ad2c91c9d00
Ranjith-create563 <ranjith@t57.ai>
2025-06-09 22:16:58 +0530

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
   return mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
   });
 };
 
